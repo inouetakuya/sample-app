217b63006e440d143ac4342efa6f44529cea4397
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-07 21:30:32 +0900

    added Help page with test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,13 @@ describe "StaticPages" do
     end
   end
 
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
   #describe "GET /static_pages" do
   #  it "works! (now write some real specs)" do
   #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
