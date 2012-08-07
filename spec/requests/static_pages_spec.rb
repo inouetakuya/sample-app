fb151cd59d93b0bbb50f1a13431137b84ec09b5b
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-07 21:34:58 +0900

    added about page with test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,13 @@ describe "StaticPages" do
     end
   end
 
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
   #describe "GET /static_pages" do
   #  it "works! (now write some real specs)" do
   #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
