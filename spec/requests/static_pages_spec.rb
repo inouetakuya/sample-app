9ba5091dc369d62244e799ab7c96fe2e294faca8
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-07 21:43:27 +0900

    improved title with test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
