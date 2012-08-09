5cf3f29c326aac8b9f1c32712a79b035cfee2827
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-09 14:32:01 +0900

    full_title についてのテストを追加した

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,6 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
 
-    #it "should have the right title" do
-    #  visit '/static_pages/home'
-    #  page.should have_selector('title',
-    #    :text => "Ruby on Rails Tutorial Sample App | Home")
-    #end
-
     it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
@@ -30,6 +24,12 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
