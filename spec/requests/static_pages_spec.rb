309b1770de6e313c61e5044c8fdaa43467c77b37
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-09 21:18:09 +0900

    subject { page } を使うようにした

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,28 @@
 require 'spec_helper'
 
+subject { page }
+
 describe "StaticPages" do
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      # visit '/static_pages/home'
-      # visit root_path
-      page.should have_selector('h1', text: 'Sample App')
-    end
 
-    it "should have the base title" do
-      # visit '/static_pages/home'
-      # visit root_path
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App")
-    end
+    #it "should have the h1 'Sample App'" do
+    #  # visit '/static_pages/home'
+    #  # visit root_path
+    #  page.should have_selector('h1', text: 'Sample App')
+    #end
+    it { should have_selector('h1', text: 'Sample App') }
+
+    #it "should have the base title" do
+    #  # visit '/static_pages/home'
+    #  # visit root_path
+    #  page.should have_selector('title',
+    #    :text => "Ruby on Rails Tutorial Sample App")
+    #end
+    it { should have_selector('title',
+      :text => "Ruby on Rails Tutorial Sample App") }
+
 
     it "should not have a custom page title" do
       # visit '/static_pages/home'
