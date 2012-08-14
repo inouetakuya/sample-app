ce7ced9f0de3278f72403473f29991cd5262ca34
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-14 12:16:09 +0900

    user authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -88,4 +89,40 @@ describe User do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+
+    # let  RSpecBook P56
+    #   find_by_email を返す「メモ化」された :found_user を定義している
+    # メモ化
+    #   メソッドが最初に呼び出されたときに戻り値がキャッシュされ、それ以降、
+    #   同じスコープ内でメソッドが呼び出されるたびにキャッシュされた値が返される
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+
+      # specify
+      #   it と同様、example のエイリアス。文脈によって使い分ける
+      specify { user_for_invalid_password.should be_false }
+    end
+
+    describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+    end
+  end
+
 end
