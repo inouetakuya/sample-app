ff9a102014087343191ef2e6fdab634178cb65b5
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-18 01:49:11 +0900

    サインインに失敗したときのテストがうまくいかない
    ブラウザでは、メールアドレスまたはパスワードが違う旨のメッセージが表示されるが、テストは失敗してしまう

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,12 @@ describe "Authentication" do
     it { should have_selector('title', text: 'Sign in') }
     it { should have_selector('div.alert.alert-error', text: 'invalid') }
 
+    describe 'after visiting another page' do
+      before { click_link 'Home' }
+
+      it { should_not have_selector('div.alert.alert-error') }
+    end
+
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -32,12 +38,6 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
-
-    describe 'after visiting another page' do
-      before { click_link 'Home' }
-
-      it { should_not have_selector('div.alert.alert-error') }
     end
   end
 
