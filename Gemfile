123b53beba4f3ee4071ea4d6f31a207f6177b215
INOUE Takuya <inouetakuya5@gmail.com>
2012-08-21 18:02:23 +0900

    pagination

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :test do
   gem 'guard-spork', '0.3.2'
   gem 'spork', '0.9.0'
   gem 'factory_girl_rails', '1.4.0'
+  gem 'database_cleaner'  # FactoryGirl の sequence に必要
 end
 
 group :production do
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
   factory :user do
-    name 'INOUE Takuya'
-    email 'inouetakuya5@gmail.com'
+    sequence(:name){ |n| "Person#{n}" }
+    sequence(:email){ |n| "person_#{n}@example.com" }
     password 'foobar'
     password_confirmation 'foobar'
   end
+
+  #factory :user do
+  #  name 'INOUE Takuya'
+  #  email 'inouetakuya5@gmail.com'
+  #  password 'foobar'
+  #  password_confirmation 'foobar'
+  #end
 end
